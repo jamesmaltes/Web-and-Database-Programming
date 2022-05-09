7c5ebcec07671eef57e0a53afd85ae1ed0f38289
James <james@themedepot.com>
2022-05-09 18:44:50 -0400

    added db_connect, mySQL functionality to user model functions, added reservation model and route as placeholder

--- public/server/routes/user.js
+++ public/server/routes/user.js
@@ -2,13 +2,52 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 
-router.get('/', (req, res) => {
- try {
- const users = User.getUsers();
- res.send(users);
- } catch(err) {
- res.status(401).send({message: err.message});
- }
-});
+router
+  .get('/', async (req, res) => {
+    try {
+      const users = await User.getUsers();
+      res.send(users);
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
 
+  .post('/login', async (req, res) => {
+    try {
+      const user = await User.login(req.body.username, req.body.password);
+      res.send({...user, password: undefined});
+    } catch (error) {
+      res.status(401).send({message: error.message});
+    }
+  })
+
+  .post('/register', async (req, res) => {
+    try {
+      const user = await User.register(req.body);
+      console.log(user)
+      res.send({...user, password: undefined})
+    } catch(error) {
+      res.status(401).send({message: error.message});
+    }
+  })
+
+  .delete('/delete', async (req, res) => {
+    try {
+      await User.deleteUser(req.body.userId);
+      res.send({success: "We'll miss you...:("});
+    } catch(error) {
+      res.status(401).send({message: error.message});
+    }
+  })
+
+  .put('/edit', async (req, res) => {
+    try {
+      const user = await User.editUser(req.body);
+      console.log(user)
+      res.send({...user, password: undefined});
+    } catch(error) {
+      res.status(401).send({message: error.message})
+    }
+  })
+  
 module.exports = router;
